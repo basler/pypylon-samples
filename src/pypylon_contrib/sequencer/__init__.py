439ff5874d9b05571a2d1e715bf14e215f11ccf6
GERMANY\SKoell <Sebastian.Koell@baslerweb.com>
2025-06-16 23:08:57 +0200

    relocate notebooks, rename utils into pypylon_contribs add pyproject.toml

---
+++ src/pypylon_contrib/sequencer/__init__.py
@@ -0,0 +1,3 @@
+"""Sequencer Control Class for Basler Cameras Ace2/BoostR"""
+
+from .sequencer import CameraSequence, SequencerTransition, SinglePathSet
--- src/pypylon_contrib/serial_communication/__init__.py
+++ src/pypylon_contrib/serial_communication/__init__.py
@@ -1,8 +1,11 @@
 """Basler Serial Communication"""
 import serial
+
 from .serial_communication import BaslerSerial
 
+
 def basler_cam_uart(camera, *args, **kwargs):
+    """Create a BaslerSerial instance"""
     return BaslerSerial(camera, *args, **kwargs)
 
 serial.protocol_handler_packages.append("serial_communication")
